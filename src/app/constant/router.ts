f6fd06df6e045cb06d1d8ab04086ae8aae748b17
cuongdq <cuongdq@vnext.com.vn>
2023-12-15 19:20:23 +0700

    test env file, test block ip

--- src/app/constant/router.ts
+++ src/app/constant/router.ts
@@ -1,4 +1,5 @@
 export const ROUTERS = {
   TEST_1: '/',
   TEST_2: '/test2',
+  PAGE_403: '/403',
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,33 @@
-import { withAuth } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 import { ROUTERS } from './app/constant/router'
 
-export default withAuth({
-  pages: {
-    signIn: ROUTERS.TEST_1,
-    error: ROUTERS.TEST_1,
-  },
-})
+const middleware = (req: NextRequestWithAuth, res: NextResponse) => {
+  // block ip JP
+  let ip = req.ip ?? req.headers.get('x-real-ip')
+  const forwardedFor = req.headers.get('x-forwarded-for')
+  if (!ip && forwardedFor) {
+    ip = forwardedFor.split(',').at(0) ?? 'Unknown'
+  }
 
-export const config = { matcher: ['/test2/:path*'] }
+  if (ip?.includes('116.96.46.169')) {
+    return NextResponse.redirect(new URL(ROUTERS.PAGE_403, req.url))
+  }
+
+  // authentication
+  if (req.nextUrl.pathname === ROUTERS.TEST_2) {
+    const withAuthFunction = withAuth({
+      pages: {
+        signIn: ROUTERS.TEST_1,
+        error: ROUTERS.TEST_1,
+      },
+    }) as Function
+    return withAuthFunction(req)
+  }
+
+  return
+}
+
+export default middleware
+
+export const config = { matcher: ['/test2/:path*', '/'] }
