f6ff2956933acbf323cddbfede17c16591aed914
cuongdq <cuongdq@vnext.com.vn>
2023-12-15 19:57:08 +0700

    Fix bug cant not access to test 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,28 @@
-import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+import { NextRequestWithAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 import { ROUTERS } from './app/constant/router'
 
-const middleware = (req: NextRequestWithAuth, res: NextResponse) => {
+const middleware = async (req: NextRequestWithAuth) => {
   // block ip JP
   let ip = req.ip ?? req.headers.get('x-real-ip')
   const forwardedFor = req.headers.get('x-forwarded-for')
   if (!ip && forwardedFor) {
-    ip = forwardedFor.split(',').at(0) ?? 'Unknown'
+    ip = forwardedFor.split(',')?.at(0)?.split(':')?.at(3) ?? 'Unknown'
   }
 
-  /* if (ip?.includes('116.96.46.169')) {
+  console.log(ip)
+
+  if (ip?.includes('116.96.46.169')) {
     return NextResponse.redirect(new URL(ROUTERS.PAGE_403, req.url))
-  } */
+  }
 
   // authentication
   if (req.nextUrl.pathname === ROUTERS.TEST_2) {
-    const withAuthFunction = withAuth({
-      pages: {
-        signIn: ROUTERS.TEST_1,
-        error: ROUTERS.TEST_1,
-      },
-    }) as Function
-    return withAuthFunction(req)
+    const token = await getToken({ req })
+    if (!token) {
+      return NextResponse.redirect(new URL(ROUTERS.TEST_1, req.url))
+    }
   }
 
   return
